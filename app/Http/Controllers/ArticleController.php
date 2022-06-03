91a8e7a934d0421185c7a750509ec96ff2d9daf0
Brix-D <Shlyakhta4@gmail.com>
2022-06-03 14:39:18 +0400

    created article controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Service;
+//use App\Models\Service;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -10,29 +11,38 @@ class ArticleController extends Controller
 
     public function index(): \Illuminate\Http\JsonResponse
     {
-        $service = Service::get();
-        return response()->json(['services' => $service]);
+        // $service = Service::get();
+        // return response()->json(['services' => $service]);
+        $articles = Article::take(25)->get();
+        return response()->json($articles);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): \Illuminate\Http\JsonResponse
     {
-        //
+        if ($request->isJson()) {
+            $form = $request->json()->all();
+        } else {
+            $form = $request->request->all();
+        }
+        $attributes = ['title' => $form['title'], 'content' => $form['content'], 'user_id' => $form['userID']];
+        $article = new Article($attributes);
+        $article->save();
+        return response()->json($article, 201);
     }
 
     /**
